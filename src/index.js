ad7dc2abdd5a4336710e8bc7e0d19a097b3c8c82
Yurii Omelchuk <omelchukyuriy1998@gmail.com>
2022-05-12 00:16:22 +0300

    Todo css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; //підключили бібліотеки
+import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header';
+import TodoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
+import ItemStatusFilter from './components/item-status-filter';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const App = () => {
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+   const todoData = [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 }
+   ];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+   return (
+      <div className="todo-app">
+         <AppHeader toDo={1} done={3} />
+         <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+         </div>
+
+         <TodoList todos={todoData} />
+      </div>
+   );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'))
